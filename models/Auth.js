39f13603d7c1c9f92e8a63e9d090fff9442d7407
someshwarsholkar <someshwarsholkar22@gmail.com>
2024-09-21 21:55:16 +0530

    register new keys

--- models/Auth.js
+++ models/Auth.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose")
 
 const authSchema = new mongoose.Schema({
-    name: {type: String, required:true },
-    password: {type: String, required:true },
-    mobile: {type: String, required:true },
     email: {type: String ,required:true},
+    fname: {type: String, required:true },
+    lname: {type: String, required:true },
+    lname: {type: String, required:true },
+    dob: {type: String, required:true },
+    gender: {type: String, required:true },
+    mobile: {type: String, required:true },
+    password: {type: String, required:true },
     otp: {type: String},
     avatar: {type: String, default:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4rHk-t6N9r-UDu9KB5irEON9DwIWDX3Roig&s" },
 })
