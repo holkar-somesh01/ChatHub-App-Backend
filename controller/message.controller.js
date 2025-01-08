9a84b2095dbdc85797ef11d247991331318546e3
someshwarsholkar <someshwarsholkar22@gmail.com>
2025-01-08 16:33:47 +0530

    update message FN

--- controller/message.controller.js
+++ controller/message.controller.js
@@ -5,20 +5,37 @@ const cloudinary = require("../utils/cloudinary.config")
 
 exports.sendMessage = AsyncHandler(async (req, res) => {
     UploadMultiple(req, res, async (err) => {
+        if (err) {
+            return res.status(500).json({ message: "Error Occupied Uploading images", error: err.message })
+        }
         const { receiverId } = req.params
-        let image
-        console.log(req.file, "FILE")
-        console.log(req.files, "Files")
-
+        let image = []
         if (req.files) {
             for (let i = 0; i < req.files.length; i++) {
                 const { secure_url } = await cloudinary.uploader.upload(req.file[i].path)
-                image = secure_url
-
+                image.push(secure_url)
             }
         }
         const { message } = req.body
-        // await Message.create({ message, receiverId, senderId: req.user, image })
+        await Message.create({ message, receiverId, senderId: req.user, image })
         res.status(200).json({ message: "Message Send Success" })
+    })
+})
+exports.UpdateMessage = AsyncHandler(async (req, res) => {
+    UploadMultiple(req, res, async (err) => {
+        if (err) {
+            return res.status(500).json({ message: "Error Occupied Uploading images", error: err.message })
+        }
+        const { messageId } = req.params
+        let image = []
+        if (req.files) {
+            for (let i = 0; i < req.files.length; i++) {
+                const { secure_url } = await cloudinary.uploader.upload(req.file[i].path)
+                image.push(secure_url)
+            }
+        }
+        const { message } = req.body
+        await Message.findByIdAndUpdate(messageId, { message, image })
+        res.status(200).json({ message: "Message Update Success" })
     })
 })
