ebbd65256e0b5bf4ce05b2f9a4faa060146e4e91
someshwarsholkar <someshwarsholkar22@gmail.com>
2025-01-09 15:53:39 +0530

    Route Set

--- routes/message.routes.js
+++ routes/message.routes.js
@@ -5,6 +5,7 @@ const MessageController = require("../controller/message.controller")
 
 router
     .post("/message-send/:receiverId", MessageController.sendMessage)
+    .put("/message-update/:messageId", MessageController.UpdateMessage)
 
 
 module.exports = router
